545951261bd4ec7f11da9f31aa0b2a4d7431ee5f
prinapatel-btc <prina.patel@botreetechnologies.com>
2017-02-02 10:53:05 +0530

    small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   rolify
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true
+  enum device_type: { android: 0, ios: 1 }
 end
